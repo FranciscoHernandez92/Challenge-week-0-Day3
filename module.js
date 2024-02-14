4bc4be381a0af925711c037d2039530802c75ae4
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-02-14 18:34:15 +0100

    Initial commit

---
+++ module.js
@@ -0,0 +1,15 @@
+export { strictEquals}
+
+function strictEquals (a, b) {
+  if (Number.isNaN(a) && Number.isNaN(b)) {
+    return  'False, Rule Exception'
+  } else if (Object.is(a, -0) && Object.is(0, b) ||  Object.is(a, 0) && Object.is(-0, b)) {
+    return 'True, Rule Exception'
+  }else if(Object.is(a, b)) { 
+    return true
+  }
+  return false
+}
+
+
+
