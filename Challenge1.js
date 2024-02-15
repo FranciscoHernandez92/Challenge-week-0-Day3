f6ba3f0ad640e02f0b2ce0fe1bed8afd0ba7c0ef
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-02-15 09:28:15 +0100

    Modificed challenge

--- Challenge1.js
+++ Challenge1.js
@@ -1,10 +1,10 @@
 import { strictEquals } from "./module.js";
 
-console.log(strictEquals(1, 1));
-console.log(strictEquals(NaN, NaN))
-console.log(strictEquals(0, -0))
-console.log(strictEquals(-0, 0));
-console.log(strictEquals(1, '1'));
-console.log(strictEquals(true, false));
-console.log(strictEquals(false, false));
-console.log(strictEquals('water', 'oil'))
+console.log('testing 1 v 1: ', strictEquals(1, 1));
+console.log('testing NaN v NaN: ',strictEquals(NaN, NaN))
+console.log('testing 0 v -0: ',strictEquals(0, -0))
+console.log('testing -0 v 0: ',strictEquals(-0, 0));
+console.log('testing 1 v "1": ',strictEquals(1, '1'));
+console.log('testing true v false: ',strictEquals(true, false));
+console.log('testing false v false: ',strictEquals(false, false));
+console.log('testing water v oil: ',strictEquals('water', 'oil'))
